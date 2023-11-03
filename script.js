7b20491a1142d44135d70595966a57df5be9fa56
arkarunkumar1221@gmail.com <arkarunkumar1221@gmail.com>
2023-11-03 17:45:02 +0530

    Bharat Intern Task-2 Completed

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function addProject() {
+    const projectName = document.getElementById("project-name").value;
+    if (projectName) {
+        const projectList = document.getElementById("project-list");
+        const projectItem = document.createElement("li");
+        projectItem.textContent = projectName;
+        projectList.appendChild(projectItem);
+        document.getElementById("project-name").value = "";
+    }
+}
+
+function addTask() {
+    const taskName = document.getElementById("task-name").value;
+    if (taskName) {
+        const taskList = document.getElementById("task-list");
+        const taskItem = document.createElement("li");
+        taskItem.textContent = taskName;
+        taskList.appendChild(taskItem);
+        document.getElementById("task-name").value = "";
+    }
+}
 
